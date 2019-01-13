3fce95f184cc136cc0164dd895119790aeda0250
cococold27 <enricogunawan27@gmail.com>
2019-01-13 12:06:00 +0700

    Initial commit

---
+++ resources/js/dogBreedGame.js
@@ -0,0 +1,14 @@
+//alert("CONNEDTED!");
+let pictures = [
+  "url(img/dog1.jpg)",
+];
+
+let squares = document.querySelectorAll(".picture");
+
+for (let i = 0; i < pictures.length; i++) {
+//  squares[i].style.backgroundImage = pictures[i];
+}
+
+//squares[2].style.backgroundImage = "url(resources/img/dog1square.jpg)";
+//squares[3].style.backgroundColor = "rgb(255, 0, 0)";
+//squares[4].style.backgroundImage = "url(img/hero.jpg)";

75a7bd333a8bc5a9245f03eb6f05ed9eddcc3fc4
cococold27 <enricogunawan27@gmail.com>
2019-01-13 17:50:25 +0700

    change p to div

--- resources/js/dogBreedGame.js
+++ resources/js/dogBreedGame.js
@@ -6,9 +6,16 @@ let pictures = [
 let squares = document.querySelectorAll(".picture");
 
 for (let i = 0; i < pictures.length; i++) {
-//  squares[i].style.backgroundImage = pictures[i];
+  squares[i].style.backgroundImage = pictures[i];
 }
 
-//squares[2].style.backgroundImage = "url(resources/img/dog1square.jpg)";
+squares[2].style.backgroundImage = "url(resources/img/dog1.jpg)";
 //squares[3].style.backgroundColor = "rgb(255, 0, 0)";
 //squares[4].style.backgroundImage = "url(img/hero.jpg)";
+
+
+
+/* NOT WORKING!!! */
+//var img = document.createElement('img');
+//img.src = 'resources/img/dog2.jpg';
+//document.getElementById('picture-container').appendChild(img);

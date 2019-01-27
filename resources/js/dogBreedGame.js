208cf78200fcdc288c1cf4247ff5726888b3f7e6
cococold27 <enricogunawan27@gmail.com>
2019-01-28 01:08:30 +0700

    add simple DOM

--- resources/js/dogBreedGame.js
+++ resources/js/dogBreedGame.js
@@ -1,17 +1,26 @@
 //alert("CONNEDTED!");
 let dogImages = [
-  "url(\"resources/img/dog1.jpg\")",
-  "url(\"resources/img/dog2.jpg\")",
-  "url(\"resources/img/dog3.jpg\")",
-  "url(\"resources/img/dog4.jpg\")",
-  "url(\"resources/img/dog5.jpg\")",
-  "url(\"resources/img/dog6.jpg\")",
+  "url(\"resources/img/dogs/dog1.jpg\")",
+  "url(\"resources/img/dogs/dog2.jpg\")",
+  "url(\"resources/img/dogs/dog3.jpg\")",
+  "url(\"resources/img/dogs/dog4.jpg\")",
+  "url(\"resources/img/dogs/dog5.jpg\")",
+  "url(\"resources/img/dogs/affenpinscher.jpg\")",
 ];
 
 let dogs = document.querySelectorAll(".picture-container");
-let answer = dogImages[2];
-let dogBreed = document.getElementById("dogBreed");
-dogBreed.textContent = answer;
+let answer = dogImages[5];
+let dogBreed = document.getElementById("dog-breed");
+
+const startIndex = "/dogs/";
+const endIndex = ".";
+let dogDisplay = answer.slice(answer.indexOf(startIndex) + startIndex.length, answer.indexOf(endIndex));
+let capitalize = dogDisplay.charAt(0).toUpperCase() + dogDisplay.substr(1);
+
+console.log(dogDisplay);
+let messageDisplay = document.querySelector("#message");
+
+dogBreed.textContent = capitalize;
 
 for (let i = 0; i < dogImages.length; i++) {
   // Add initial images
@@ -21,6 +30,10 @@ for (let i = 0; i < dogImages.length; i++) {
     // alert("clicked an image");
     let clickedImage = this.style.backgroundImage;
     console.log(clickedImage + " " + answer);
-    (clickedImage === answer) ? alert("Correct!") : alert("Wrong!");
+    if (clickedImage === answer) {
+      messageDisplay.textContent = "Correct";
+    } else { 
+      messageDisplay.textContent = "Try again";
+    } 
   });
 }
